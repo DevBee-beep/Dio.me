f9519d49ebcc4a34300ff0bdb83209780578acf0
Bee <122950577+DevBee-beep@users.noreply.github.com>
2023-11-22 08:54:23 +0000

    primeiro código

---
+++ Desenvolvimento Backend com Kotlin/Introdução Prática à Linguagem de Programação Kotlin/Exemplos Práticos/Olá, Mundo!/ola_mundo.kt
@@ -0,0 +1,6 @@
+package org.kotlinlang.play     // 1. Definição do pacote (algo muito comum para organização de código)
+//package me.dio.helloworld
+fun main() {					// 2. Ponro de entrada de um código kotlin, definido pela função (main)
+    print("Olá, mundo")			// não pula linha (só c ln pula)
+    println("Hello, world!!!")	// 3. Imprime na saída padrão um texto, pulando Linha
+}								
